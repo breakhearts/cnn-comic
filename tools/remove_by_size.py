386d91a8e0d38efc97d8aeb3e461854dcb5998a9
breakhearts <lxx1234@gmail.com>
2016-06-18 14:42:55 +0800

    =

---
+++ tools/remove_by_size.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+from PIL import Image
+import numpy as np
+import os
+import shutil
+
+
+def wise_mk_dir(path):
+    if path == "":
+        return
+    if os.path.exists(path):
+        return
+    p, c = os.path.split(path)
+    if not os.path.exists(p):
+        wise_mk_dir(p)
+    os.mkdir(path)
+
+
+def wise_mk_dir_for_file(filepath):
+    p = os.path.dirname(filepath)
+    wise_mk_dir(p)
+
+
+def is_remove(fname):
+    try:
+        img = Image.open(fname)
+        img = img.convert("HSV")
+    except:
+        return True
+    return img.size[0] < 256 or img.size[1] < 256
+
+
+def split_photo(img_root, photo_root):
+    for p, _ , files in os.walk(img_root):
+        for fname in files:
+            ext = fname.split(".")[-1].lower()
+            if ext not in ["jpg","png"]:
+                continue
+            fp = os.path.join(p, fname)
+            print fp
+            if is_remove(fp):
+                dst = os.path.dirname(os.path.join(photo_root, os.path.relpath(fp, img_root)))
+                wise_mk_dir(dst)
+                print "Move {0} to {1}".format(fp, dst)
+                shutil.move(fp, dst)
+
+split_photo("d:\data", "d:\data_photo")
